914596a402b76873f4af09440d15bd775b66f6df
Kamil <s.kamilx@gmail.com>
2025-03-08 07:02:08 +0100

    feature: Implementation of start game feature

--- src/main/java/org/example/ScoreBoard.java
+++ src/main/java/org/example/ScoreBoard.java
@@ -1,3 +1,9 @@
 package org.example;
 
-public class ScoreBoard {}
+public class ScoreBoard {
+
+    public void startGame(Game game) {
+        game.setHomeTeamScore(0);
+        game.setAwayTeamScore(0);
+    }
+}
